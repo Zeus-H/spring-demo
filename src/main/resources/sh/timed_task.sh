0882aadd4a09a15c577fbde5c471503cc259149a
huangliyuan <hlyuan1994@163.com>
2023-03-27 15:10:35 +0800

    demo

--- src/main/resources/sh/timed_task.sh
+++ src/main/resources/sh/timed_task.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/Zeus-H/spring-demo/master/src/main/resources/sh/clean_logs.sh
+wget https://raw.githubusercontent.com/Zeus-H/spring-demo/master/src/main/resources/sh/clean_demo.sh
 echo "下载脚本..."
 
 chmod +x clean_logs.sh
+chmod +x clean_demo.sh
 echo "设置权限..."
+
+echo /root/test.txt
 
 echo "0 1 * * * /root/clean_logs.sh" | sudo crontab -
 echo "设置定时任务..."
