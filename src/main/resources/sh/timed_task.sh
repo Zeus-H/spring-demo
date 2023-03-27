afb5f658d2a98a8af803851f777b0d42a291343b
huangliyuan <hlyuan1994@163.com>
2023-03-27 11:26:59 +0800

    新增脚本

---
+++ src/main/resources/sh/timed_task.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/Zeus-H/spring-demo/master/src/main/resources/sh/clean_logs.sh
+echo "下载脚本..."
+
+chmod +x clean_logs.sh
+echo "设置权限..."
+
+echo "0 1 * * * /root/clean_logs.sh" | sudo crontab -
+echo "设置定时任务..."
