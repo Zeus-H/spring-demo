b7f540c744c2b5f589dc9c7df09b4907e6d6f6bc
huangliyuan <hlyuan1994@163.com>
2024-09-05 14:44:47 +0800

    2024年9月5日

--- src/test/java/com/example/springdemo/service/demo/impl/Demo.java
+++ src/test/java/com/example/springdemo/service/demo/impl/Demo.java
@@ -1,12 +1,26 @@
 package com.example.springdemo.service.demo.impl;
 
 /**
+ * 冒泡排序
+ *
  * @author Mr.Huang
  * @date 2023/4/3 17:44
  **/
 public class Demo {
     public static void main(String[] args) {
-        //打印打印
-
+        // 冒泡排序
+        int[] arr = {1, 3, 2, 5, 4};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
 }
